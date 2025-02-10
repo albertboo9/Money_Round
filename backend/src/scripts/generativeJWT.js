6982be920450ce4ef0a67433047bdce4751db242
SonnyHardy <htambeck@gmail.com>
2025-02-10 12:26:57 +0100

    Ajustements

--- backend/src/scripts/generativeJWT.js
+++ backend/src/scripts/generativeJWT.js
@@ -3,9 +3,12 @@ require("dotenv").config();
 const jwt = require('jsonwebtoken');
 
 const payload = {
-  userId: 'user123',
+  uid: 'user123',
+  fullName: 'Doe',
+  email: 'doe@example.com',
+  phoneNumber: +2376899444,
   // Ajoutez d'autres informations nécessaires
 };
 
-const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1m' });
+const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '4m' });
 console.log(token);
