1b29dd69458dc5ae48b19e9d2e91668195853da1
albertboo9 <booalbert60@gmail.com>
2025-02-15 19:54:31 +0100

    news

--- frontend/MoneyRound/src/main.jsx
+++ frontend/MoneyRound/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+//import App from './App.jsx'
+import Login from './page/Login.jsx'
+/* import Createaccount from './page/Createaccount.jsx' */
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+   {/*  <Createaccount/> */}
+    <Login/>
+  </StrictMode>
 )
