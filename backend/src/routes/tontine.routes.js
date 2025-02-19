ed27c31c5814d36383a5f3cd1240f235ee608e05
albertboo9 <booalbert60@gmail.com>
2025-02-19 11:04:09 +0100

    router mis à jour

--- backend/src/routes/tontine.routes.js
+++ backend/src/routes/tontine.routes.js
@@ -1,30 +1,50 @@
-const tontineCtrl = require('../controllers/tontine.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
-const express = require('express');
-const checkTourStatus = require('../middlewares/checkTourStatus');
+const tontineCtrl = require("../controllers/tontine.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
+const express = require("express");
+const checkTourStatus = require("../middlewares/checkTourStatus");
 const router = express.Router();
-const verifyApiKey=require('../config/verifyApikey')
+const verifyApiKey = require("../config/verifyApikey");
 
-router.use(verifyApiKey)
+router.use(verifyApiKey);
 // Créer une nouvelle tontine
-router.post('/create', authMiddleware(),  tontineCtrl.createTontine);
+router.post("/create", authMiddleware(), tontineCtrl.createTontine);
 
 // Mettre à jour une tontine existante
-router.put('/update/:tontineId', authMiddleware(),  tontineCtrl.updateTontine);
+router.put("/update/:tontineId", authMiddleware(), tontineCtrl.updateTontine);
 
 // Supprimer une tontine
-router.delete('/delete/:tontineId',  authMiddleware(),  tontineCtrl.deleteTontine);
+router.delete(
+  "/delete/:tontineId",
+  authMiddleware(),
+  checkTourStatus,
+  tontineCtrl.deleteTontine
+);
 
 // Récupérer une tontine par son ID
-router.get('/:tontineId', authMiddleware(),  tontineCtrl.getTontineById);
+router.get("/:tontineId", authMiddleware(), tontineCtrl.getTontineById);
 
 // Nommer un administrateur de tontine
-router.put('/admin/:tontineId',  authMiddleware(),  tontineCtrl.makeAdmin);
+router.put(
+  "/admin/:tontineId",
+  authMiddleware(),
+  checkTourStatus,
+  tontineCtrl.makeAdmin
+);
 
 // Rejoindre une tontine
-router.post('/join/:tontineId',  authMiddleware(),  tontineCtrl.joinTontine);
+router.post(
+  "/join/:tontineId",
+  authMiddleware(),
+  checkTourStatus,
+  tontineCtrl.joinTontine
+);
 
 // Inviter un membre à rejoindre une tontine
-router.post('/invite/:tontineId', authMiddleware(),  tontineCtrl.inviteMember);
+router.post(
+  "/invite/:tontineId",
+  authMiddleware(),
+  checkTourStatus,
+  tontineCtrl.inviteMember
+);
 
 module.exports = router;
