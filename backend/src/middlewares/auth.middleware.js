3bddacb8480bd52877efe7a87b924baa1d16f5b4
albertboo9 <booalbert60@gmail.com>
2025-02-22 19:46:49 +0100

    configuration de sécurité

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -12,6 +12,7 @@ module.exports = () => async (req, res, next) => {
 
     const token = authHeader.split(" ")[1];
     req.user = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(req.user);
     next();
 
     // Vérification et décodage du JWT
