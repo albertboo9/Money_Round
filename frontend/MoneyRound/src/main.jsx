e4d0a172bb14b0fd6fbf6d4456b6fb357285c15f
albertboo9 <booalbert60@gmail.com>
2025-02-25 13:43:56 +0100

    route

--- frontend/MoneyRound/src/main.jsx
+++ frontend/MoneyRound/src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 //import App from './App.jsx'
 import Login from './page/Login.jsx'
-/* import Createaccount from './page/Createaccount.jsx' */
+import Createaccount from './page/Createaccount.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   {/*  <Createaccount/> */}
+    <Createaccount/>
     <Login/>
   </StrictMode>
 )
